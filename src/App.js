d483bc133dde194bcf8b2241bcf45cd52266a43b
Vitor Toledo <vitortoledorodovalho@gmail.com>
2023-04-18 23:10:04 -0300

    inclusao botao editar e incluir form atividade

--- src/App.js
+++ src/App.js
@@ -4,24 +4,58 @@ import './App.css';
 let initialState = [
   {
     id: 1,
+    prioridade: '1',
+    titulo: 'Título',
     descricao: 'Primeira Atividade',
   },
   {
     id: 2,
+    prioridade: '1',
+    titulo: 'Título',
     descricao: 'Segunda Atividade',
   }
 ];
 
 function App() {
   const [atividades, setAtividades] = useState(initialState)
+  const [id, updateId] = useState(3)
 
   function addAtividade(e){
     e.preventDefault();
+    document.getElementById('id').value = parseInt(document.getElementById('id').value) + 1;
     const atividade = {
       id: document.getElementById('id').value,
+      prioridade: document.getElementById('prioridade').value,
+      titulo: document.getElementById('titulo').value,
       descricao: document.getElementById('descricao').value,
     }
     setAtividades([...atividades, {...atividade}]);
+  }
+
+  function prioridadeLabel(param){
+    switch(param){
+      case '1':
+        return 'Baixa';
+      case '2':
+        return 'Normal';
+      case '3':
+        return 'Alta';
+      default:
+        return 'Não definido';
+    }
+  }
+
+  function prioridadeStyle(param, icone){
+    switch(param){
+      case '1':
+        return icone ? 'smile' : 'success';
+      case '2':
+        return icone ? 'meh' : 'dark';
+      case '3':
+        return icone ? 'frown' : 'warning';
+      default:
+        return 'Não definido';
+    }
   }
 
   return (
@@ -29,7 +63,22 @@ function App() {
       <form className='row g-3'>
         <div className="col-md-6">
           <label for="id" className="form-label">ID</label>
-          <input type="text" className="form-control" id="id" />
+          <input type="text" className="form-control" id="id" value="3" readOnly />
+        </div>
+
+        <div className='col-md-6'>
+          <label className='form-label'>Prioridade</label>
+          <select id="prioridade" className="form-select">
+            <option defaultValue="0" selected>Selecionar ...</option>
+            <option value="1">Baixa</option>
+            <option value="2">Normal</option>
+            <option value="3">Alta</option>
+          </select>
+        </div>
+
+        <div className="col-md-6">
+          <label for="titulo" className="form-label">Título</label>
+          <input type="text" className="form-control" id="titulo" />
         </div>
 
         <div className="col-md-6">
@@ -43,20 +92,28 @@ function App() {
       </form>
       <div className="mt-3">
           {atividades.map(ativ => (
-            <div key={ativ.id} className="card mb-2 shadow-sm">
+            <div key={ativ.id} className={"card mb-2 shadow-sm border-" + prioridadeStyle(ativ.prioridade)}>
               <div className="card-body">
                 <div className="d-flex justify-content-between">
                   <h5 className='card-title'>
                     <span className="badge bg-secondary rounded-pill text-bg-primary me-1">
                       {ativ.id}
                     </span>
-                    - título
+                    - {ativ.titulo}
                   </h5>
                   <h6>
-                    Prioridade: Normal
+                    Prioridade: {prioridadeLabel(ativ.prioridade)}
                   </h6>
                 </div>
                 <p className="card-text">{ativ.descricao}</p>
+                <div className='d-flex justify-content-end pt-2 m-0 border-top'>
+                  <button className='btn btn-sm btn-outline-primary me-2'>
+                    Editar
+                  </button>
+                  <button className='btn btn-sm btn-outline-danger'>
+                    Deletar
+                  </button>
+                </div>
               </div>
             </div>
           ))}
